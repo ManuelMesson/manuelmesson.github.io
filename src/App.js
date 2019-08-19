d00ccf0f39c516435b22e39b9e722cf3a9882074
Manuel Messon-Roque <manuelmesson@gmail.com>
2019-08-18 17:28:52 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import './App.css';
-import Avatar from '@material-ui/core/Avatar';
+import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import About from './components/About';
-import Projects from './components/Projects';
+import Main from "./containers/Main";
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="./profile-pic.jpg" className="App-logo" alt="profile-pic" />
-        <Router>
-        <Link to="about">About</Link>
-        <Link to="personal-projects">Personal Projects</Link>
-          <Route path="/about" component={About}></Route>
-          <Route path="/personal-projects" component={Projects}></Route>
-        </Router>
+      <header>
+        <img src="./profile-pic.jpg" className="App-logo circle" alt="profile-pic" />
       </header>
+      <Router>
+          <Row className="d-flex align-items-center">
+          <Col xs={3}><h1><Link to="/personal-projects">Work Experience</Link></h1></Col> 
+            <Col xs={3}><h1><Link to="/personal-projects">Personal Projects</Link></h1></Col> 
+            <Col xs={3}><h1><Link to="/about">Resume</Link></h1></Col> 
+            <Col xs={3}><h1><Link to="/about">About</Link></h1></Col> 
+          </Row> 
+        <Main />
+      </Router>
     </div>
   );
 }
---
+++ src/containers/Main.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import About from "./../components/About";
+import Projects from "./../components/Projects";
+import Container from 'react-bootstrap/Container';
+class Main extends Component {
+  render = () => {
+    return (
+      <Container>
+          <Route path="/personal-projects" component={Projects} />
+          <Route path="/resume" component={Projects} />
+          <Route path="/about" component={About} />
+      </Container>
+    );
+  };
+}
+
+export default Main;
